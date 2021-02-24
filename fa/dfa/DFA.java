4ef1897ecb59ea2d8d535e688144246cebad873c
JeremyBouchard220 <60022660+JeremyBouchard220@users.noreply.github.com>
2021-02-24 09:33:45 -0700

    More attempts to fix things

---
+++ fa/dfa/DFA.java
@@ -0,0 +1,27 @@
+package fa.dfa;
+
+import fa.dfa.DFAInterface;
+import fa.dfa.DFAState;
+import fa.dfa.State;
+
+public class DFA implements DFAInterface {
+
+    @Override
+    public boolean accepts(String s) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public State getToState(DFAState from, char onSymb) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public State getToState(fa.dfa.DFAState from, char onSymb) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+        
+}
