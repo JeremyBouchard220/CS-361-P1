25defb0636b2f01a38d80110ffec2cc263e243b3
JeremyBouchard220 <60022660+JeremyBouchard220@users.noreply.github.com>
2021-02-24 09:36:56 -0700

    DFA.java Update #1
    Packages have been fixed! File paths due to improperly located files are the result of the problems I was having. All implemented methods are now properly added to DFA.java

--- fa/dfa/DFA.java
+++ fa/dfa/DFA.java
@@ -1,25 +1,77 @@
 package fa.dfa;
 
+import java.util.Set;
 import fa.dfa.DFAInterface;
 import fa.dfa.DFAState;
-import fa.dfa.State;
 
 public class DFA implements DFAInterface {
 
     @Override
-    public boolean accepts(String s) {
+    public boolean accepts(String s) 
+    {
         // TODO Auto-generated method stub
         return false;
     }
 
     @Override
-    public State getToState(DFAState from, char onSymb) {
+    public void addStartState(String name) 
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void addState(String name) 
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void addFinalState(String name) 
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void addTransition(String fromState, char onSymb, String toState) 
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Set<? extends fa.State> getStates() 
+    {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public State getToState(fa.dfa.DFAState from, char onSymb) {
+    public Set<? extends fa.State> getFinalStates() 
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public fa.State getStartState() 
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Set<Character> getABC() 
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public fa.State getToState(DFAState from, char onSymb) 
+    {
         // TODO Auto-generated method stub
         return null;
     }
