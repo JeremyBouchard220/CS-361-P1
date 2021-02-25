86bf0abb47d6e83da681501feb19613a7eebda9a
JeremyBouchard220 <60022660+JeremyBouchard220@users.noreply.github.com>
2021-02-25 15:03:26 -0700

    DFA.java Update #3
    Added Comments above imported methods

--- fa/dfa/DFA.java
+++ fa/dfa/DFA.java
@@ -2,72 +2,109 @@ package fa.dfa;
 
 import java.util.Set;
 
-public class DFA implements DFAInterface {
+public class DFA implements DFAInterface 
+{
 
-    @Override
+    /**
+	 * Simulates a DFA on input s to determine
+	 * whether the DFA accepts s.
+	 * @param s - the input string
+	 * @return true if s in the language of the DFA and false otherwise
+	 */
     public boolean accepts(String s) 
     {
         // TODO Auto-generated method stub
         return false;
     }
 
-    @Override
+    /**
+	 * Adds the initial state to the DFA instance
+	 * @param name is the label of the start state
+	 */
     public void addStartState(String name) 
     {
         // TODO Auto-generated method stub
 
     }
 
-    @Override
+    /**
+	 * Adds a non-final, not initial state to the DFA instance
+	 * @param name is the label of the state 
+	 */
     public void addState(String name) 
     {
         // TODO Auto-generated method stub
 
     }
 
-    @Override
+    /**
+	 * Adds a final state to the DFA
+	 * @param name is the label of the state
+	 */
     public void addFinalState(String name) 
     {
         // TODO Auto-generated method stub
 
     }
 
-    @Override
+    /**
+	 * Adds the transition to the DFA's delta data structure
+	 * @param fromState is the label of the state where the transition starts
+	 * @param onSymb is the symbol from the DFA's alphabet.
+	 * @param toState is the label of the state where the transition ends
+	 */
     public void addTransition(String fromState, char onSymb, String toState) 
     {
         // TODO Auto-generated method stub
 
     }
 
-    @Override
+    /**
+	 * Getter for Q
+	 * @return a set of states that FA has
+	 */
     public Set<? extends fa.State> getStates() 
     {
         // TODO Auto-generated method stub
         return null;
     }
 
-    @Override
+    /**
+	 * Getter for F
+	 * @return a set of final states that FA has
+	 */
     public Set<? extends fa.State> getFinalStates() 
     {
         // TODO Auto-generated method stub
         return null;
     }
 
-    @Override
+    /**
+	 * Getter for q0
+	 * @return the start state of FA
+	 */
     public fa.State getStartState() 
     {
         // TODO Auto-generated method stub
         return null;
     }
 
-    @Override
+    /**
+	 * Getter for Sigma
+	 * @return the alphabet of FA
+	 */
     public Set<Character> getABC() 
     {
         // TODO Auto-generated method stub
         return null;
     }
 
-    @Override
+    /**
+	 * Uses transition function delta of FA
+	 * @param from the source state
+	 * @param onSymb the label of the transition
+	 * @return the sink state.
+	 */
     public fa.State getToState(DFAState from, char onSymb) 
     {
         // TODO Auto-generated method stub
